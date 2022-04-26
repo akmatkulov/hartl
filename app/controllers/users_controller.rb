910bf30050ebdbbda3df56647ae8fb7c01d7a590
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2022-04-26 07:48:10 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        # Обработать усешное сохранение 
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
     else
-        render :new, status: :unprocessable_entity
+        render :new
     end 
   end 
 
